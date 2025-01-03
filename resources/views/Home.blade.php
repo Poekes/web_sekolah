86f9a3e6bb9391ff61d7d1ba8d80a34cd001f587
Poekes <sunday26261@gmail.com>
2025-01-03 15:10:02 +0700

    style: footer responsive

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -15,28 +15,21 @@
     @vite('resources/js/app.js')
 </head>
 
-<body class="w-full "
-      x-data="{
-          open: false,
-          toggle() { this.open = !this.open }
-      }">
-    <div class="w-full min-w-[300px]  ">
-        <x-Navbar></x-Navbar>
-        <x-Hero-section> </x-Hero-section>
-        <button class="btn"
-                @click="toggle()">buka</button>
-    </div>
+<body class="w-full">
+    <x-Navbar></x-Navbar>
+    <x-Hero-section></x-Hero-section>
+    <div class="w-full min-w-[300px] max-w-[850px] border m-auto">
 
-    <div class="w-[100%] px-5 py-2  flex-col"
-         x-show="open">
 
-        <div class="my-2 border shadow-md mockup-window bg-base-300">
-            <div class="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+        <div class="w-[100%] px-5 py-2 flex-col">
+            <div class="my-2 border shadow-md mockup-window bg-base-300">
+                <div class="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+            </div>
+
         </div>
-
     </div>
 
     <x-Footer></x-Footer>
 </body>
 
-</html>/
+</html>
