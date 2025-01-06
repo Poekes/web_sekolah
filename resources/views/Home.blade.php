3c39f79fce87454d106ba1e3a9e853b8fbaf9f8d
Poekes <sunday26261@gmail.com>
2025-01-06 22:06:32 +0700

    feat: add sejarah at Home Page

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,7 +1,7 @@
 <x-layout :content="$content">
     <x-herosection :herocontent="$content" />
-    <div class="w-full h-20 bg-red-200 border-red-500"></div>
-    <div class="w-full min-w-[300px] max-w-[950px]  m-auto">
+    <x-sejarah />
+    <div class="w-full  max-w-[950px]  m-auto">
         <h1>Home Page</h1>
     </div>
 </x-layout>
---
+++ resources/views/components/sejarah.blade.php
@@ -0,0 +1,17 @@
+<div class="w-full hero bg-base-200 !min-w-[200px]">
+    <div class="flex-col-reverse w-full hero-content !min-w-[200px] md:flex-row-reverse lg:px-6">
+        <div class=" w-[95%]  bg-transparent rounded-lg shadow-2xl sm:w-96 sm:h-auto min-w-[40%] aspect-video">
+            <img src="{{ 'storage/fotosmk.jpg' }}"
+                 class="object-cover w-full h-full rounded-md !min-w-[200px]" />
+        </div>
+        <div class="w-full min-w-[200px]">
+            <h1 class="text-4xl font-bold sm:text-5xl">Sejarah</h1>
+            <p class="w-full py-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non beatae ipsa dignissimos, recusandae cum
+                mollitia voluptatum! Aperiam rerum totam deleniti ducimus veritatis illum magnam error provident
+                delectus dicta facilis harum suscipit, rem tempore consectetur minus quas aliquid repudiandae, explicabo
+                nisi alias velit enim, aspernatur facere. Voluptatem architecto magni consequatur non?
+            </p>
+        </div>
+    </div>
+</div>
